cf18153a7ccb3e10b63e1eb79187e2d5d22145da
nathaniaearlene <nathaniaearlene08@gmail.com>
2026-04-08 14:24:28 +0700

    php

---
+++ pertemuan13/Mobil.php
@@ -0,0 +1,46 @@
+<?php
+namespace Kendaraan;
+//Cara penulisan class mobil
+class Mobil{
+    //Cara penulisan property
+    public $warna;
+    public $merk;
+    //cara penulisan method
+    function maju () {
+        //isi method maju
+        return "Mobil maju";
+    }
+
+    function mundur() {
+        //isi method berhenti
+        return "Mobil mundur";
+    }
+}
+
+//Cara menggunakan namespace
+// use Kendaraan\Mobil;
+//membuat inisial namespace
+use Kendaraan\Mobil as kMobil;
+
+//inisialisasi object dari namespace alias
+$mobil_ahmad = new kMobil();
+
+//inisialisasi object
+// $mobil_ahmad = new Mobil();
+$mobil_anton = new Mobil();
+
+// set property
+$mobil_ahmad->warna = "Hitam";
+$mobil_ahmad->merk = "Toyota";
+
+//tampilkan property
+echo "Mobil ahnad";
+echo "<br>warna: ". $mobil_ahmad->warna;
+echo "<br>merk: ". $mobil_ahmad->merk;
+
+//tampilkan method
+echo $mobil_ahmad->maju();
+echo "<br>";
+echo $mobil_ahmad->berenti();
+
+
