8b237fdf0ae4a85af71d6a365a464989e2130963
nathaniaearlene <nathaniaearlene08@gmail.com>
2026-04-08 13:55:24 +0700

    php

---
+++ pertemuan12/proses.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Materi Dasar PHP Form - POST</title>
+</head>
+<body>
+    <h2>Contoh Proses Form POST</h2>
+    <?php if (!empty($postErrors)): ?>
+        <div class= "error">
+            <strong>Validasi gagal:</strong>
+        </div>
+
+</body>
+</html>
