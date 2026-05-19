71d71ca80a2ef45a8ee27bdcde365925aead3fb1
nathaniaearlene <nathaniaearlene08@gmail.com>
2026-05-19 11:56:08 +0700

    laravel

--- NathaniaEarlene/resources/views/produk/create.blade.php
+++ NathaniaEarlene/resources/views/produk/create.blade.php
@@ -1,3 +1,26 @@
-<div>
-    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
+@extends('app.master')
+
+@section('title', $title)
+
+@section('sidebar')
+    @parent
+    @section('submenu-produk')
+        <a href="/produk/create" class="list-group-item list-group-item-action ps-4>">Tambah Produk</a>
+        <a href="/produk/search" class="list-group-item list-group-item-action ps-4>">Cari Produk</a>
+    @endsection
+
+@endsection
+
+@section('content')
+<div class="container-fluid">
+    <h1 class="mb-4">{{ $title }}</h1>
+        <p>Nama Produk: <input type="text" class="form-control mb-3" placeholder="tambah produk..."> <p>
+        <p>Price: <input type="number" class="form-control mb-3" placeholder="tambah harga..."> <p>
+        <p>Description: <input type="text" class="form-control mb-3" placeholder="tambah deskripsi..."> <p>
+        <p>Status: <input type="text" class="form-control mb-3" placeholder="tambah status..."> <p>
+        <p>Aktif: <input type="text" class="form-control mb-3" placeholder="tambah aktif..."> <p>
+        <p>Tanggal Rilis: <input type="text" class="form-control mb-3" placeholder="tambah tanggal rilis..."> <p>
+        <button class="btn btn-primary">Simpan</button>
+    <hr>
 </div>
+@endsection
